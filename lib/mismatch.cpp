e1ad825e41e7961ee1f64b17b499d45fdc3937b2
Rémi Lespinet <remi@lespi.net>
2018-03-10 18:33:24 +0100

    Mismatch kernel : Improve performances (~x4)

--- lib/mismatch.cpp
+++ lib/mismatch.cpp
@@ -2,23 +2,29 @@
 #include <vector>
 #include <cstring>
 
-#include "spectrum.hpp"
+#include "kmer.hpp"
+#include "mismatch.hpp"
 #include "utils.hpp"
 
 #include "debug.hpp"
 
-template<class K, class V>
-using map = std::map<K, V>;
-
-struct mismatch_struct_t {
-    const int *ptr;
+template<typename hash_t>
+struct kmer_data_t : kmer_count<hash_t> {
     int seq_id;
+    int mismatchs;
 };
 
-static void compute_kernel_rec(kernel_t &kernel, mismatch_struct_t* tracks,
-                               int *mismatchs, int len, int k, int d, int l) {
+template<typename hash_t>
+struct depth_cache_t {
+    kmer_decoding_key_t *decode_keys;
+    kmer_data_t<hash_t> *kmer_data;
+    int line_size;
+};
 
-    // static std::vector<int> debug;
+template<typename seq_t, typename hash_t>
+void mismatch_compute_rec(depth_cache_t<hash_t> &cache, kernel_t *kernel,
+                          const kmer_data_t<hash_t>* tracks, int len,
+                          int k, int d, int l) {
 
     if (len == 0) {
         return;
@@ -32,110 +38,137 @@ static void compute_kernel_rec(kernel_t &kernel, mismatch_struct_t* tracks,
                 int id1 = tracks[i].seq_id;
                 int id2 = tracks[j].seq_id;
 
-                kernel.data[id1 * kernel.size + id2] += 1;
-                kernel.data[id2 * kernel.size + id1] += 1;
+                int matches = tracks[i].count * tracks[j].count;
+
+                kernel->data[id1 * kernel->size + id2] += matches;
+                kernel->data[id2 * kernel->size + id1] += matches;
             }
         }
 
         return;
     }
 
-
-    int *old_mismatchs = new int[len];
-    mismatch_struct_t *old_tracks = new mismatch_struct_t[len];
+    kmer_data_t<hash_t> *new_tracks = cache.kmer_data + d * cache.line_size;
+    kmer_decoding_key_t &key = cache.decode_keys[d];
 
     for (int a = 0; a < l; a++) {
 
-        // debug.push_back(a);
-        // for (int i = 0; i < debug.size(); i++) {
-        //     std::cout << debug[i] << " ";
-        // }
+        int new_len = 0;
+        for (int i = 0; i < len; i++) {
 
-        std::memcpy(old_mismatchs, mismatchs, sizeof(int) * len);
-        std::memcpy(old_tracks, tracks, sizeof(mismatch_struct_t) * len);
+            seq_t c = kmer_decode_id<seq_t, hash_t>(tracks[i], key);
+            if (c == a || tracks[i].mismatchs > 0) {
 
-        int i = 0, j = len;
-        while (i < j) {
+                new_tracks[new_len] = tracks[i];
+                new_tracks[new_len].mismatchs -= (c == a) ? 0 : 1;
 
-            if (tracks[i].ptr[d] == a || mismatchs[i] > 0) {
-
-                if (tracks[i].ptr[d] != a) {
-                    mismatchs[i]--;
-                }
-
-                i++;
-            } else {
-                j--;
-                std::swap(tracks[i], tracks[j]);
-                std::swap(mismatchs[i], mismatchs[j]);
+                new_len++;
             }
 
         }
 
-        // std::cout << ": " << j << std::endl;
+        mismatch_compute_rec<seq_t, hash_t>(cache, kernel, new_tracks, new_len, k, d + 1, l);
 
-        compute_kernel_rec(kernel, tracks, mismatchs, j, k, d + 1, l);
-
-        std::memcpy(mismatchs, old_mismatchs, sizeof(int) * len);
-        std::memcpy(tracks, old_tracks, sizeof(mismatch_struct_t) * len);
-
-        // debug.pop_back();
 
     }
 
 }
 
 
+template<typename seq_t, typename hash_t>
+static inline bool mismatch_compute(kernel_t *kernel, const kmer_data_t<hash_t>* tracks,
+                                    int len, int k, int l) {
+
+
+    kmer_decoding_key_t *decode_keys = new kmer_decoding_key_t[k];
+    if (decode_keys == nullptr) {
+        return false;
+    }
+
+    for (int i = 0; i < k; i++) {
+        decode_keys[i] = kmer_decoding_key(i, k, l);
+    }
+
+    kmer_data_t<hash_t> *kmer_cache = new kmer_data_t<hash_t>[len * k];
+    if (kmer_cache == nullptr) {
+        delete[] decode_keys;
+        return false;
+    }
+
+    depth_cache_t<hash_t> cache = {
+        decode_keys,
+        kmer_cache,
+        len
+    };
+
+    mismatch_compute_rec<seq_t, hash_t>(cache, kernel, tracks, len, k, 0, l);
+
+    delete[] kmer_cache;
+    delete[] decode_keys;
+
+    return true;
+}
+
 kernel_t mismatch(const seq_array_t &array, int k, int m) {
 
-    const int *data = array.data;
-    const int rows = array.sequences_count;
-    const int cols = array.sequences_length;
-    const int l = array.alphabet_size;
+    using hash_t = uint64_t;
+    using seq_t = int;
 
-    float *kernel_data = new float[rows * rows]();
+    const int seq_count = array.sequences_count;
+    const int seq_len = array.sequences_length;
+    const int alphabet_size = array.alphabet_size;
+
+    float *kernel_data = new float[seq_count * seq_count]();
     if (kernel_data == nullptr) {
         return invalid_kernel;
     }
 
-    int tracks_len = rows * (cols - k + 1);
-    mismatch_struct_t *tracks = new mismatch_struct_t[tracks_len];
-    if (tracks == nullptr) {
+    const int max_kmer_len = seq_len - k + 1;
+    kmer_count<hash_t> *kmers_count = new kmer_count<hash_t>[max_kmer_len];
+    if (kmers_count == nullptr) {
+        delete[] kernel_data;
         return invalid_kernel;
     }
 
-    for (int i = 0; i < rows; i++) {
+    kmer_data_t<hash_t> *kmers_data = new kmer_data_t<hash_t>[seq_count * max_kmer_len];
+    if (kmers_data == nullptr) {
+        delete[] kmers_count;
+        delete[] kernel_data;
+        return invalid_kernel;
+    }
 
-        const int* data_i = data + cols * i;
+    // Filling in the structure
+    int kmers_len = 0;
+    for (int i = 0; i < seq_count; i++) {
+        int len = kmer_encoding_count(kmers_count, array.data + i * seq_len,
+                                      seq_len, k, alphabet_size);
 
-        for (int j = 0; j < cols - k + 1; j++) {
+        for (int j = 0; j < len; j++) {
+            (kmer_count<hash_t>&) kmers_data[kmers_len] = kmers_count[j];
 
-            tracks[i * (cols - k + 1) + j].ptr = data_i + j;
-            tracks[i * (cols - k + 1) + j].seq_id = i;
+            kmers_data[kmers_len].seq_id = i;
+            kmers_data[kmers_len].mismatchs = m;
 
+            kmers_len++;
         }
 
     }
 
-    int *mismatchs = new int[tracks_len];
-    if (mismatchs == nullptr) {
+    kernel_t kernel = {
+        kernel_data,
+        seq_count
+    };
+
+    bool ok = mismatch_compute<seq_t, hash_t>(&kernel, kmers_data, kmers_len, k, alphabet_size);
+    if (!ok) {
+        delete[] kmers_data;
+        delete[] kmers_count;
+        delete[] kernel_data;
         return invalid_kernel;
     }
 
-
-    for (int i = 0; i < tracks_len; i++) {
-        mismatchs[i] = m;
-    }
-
-    kernel_t kernel = {
-        kernel_data,
-        rows
-    };
-
-    compute_kernel_rec(kernel, tracks, mismatchs, tracks_len, k, 0, l);
-
-    delete[] tracks;
-    delete[] mismatchs;
+    delete[] kmers_data;
+    delete[] kmers_count;
 
     return kernel;
 }
