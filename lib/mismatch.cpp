182645c7f6ce6fe2f4460cbde7ea17a53be20362
Rémi Lespinet <remi@lespi.net>
2018-03-12 23:03:51 +0100

    Add a small trick to improve mismatch kernel computation
    Example
    
    | i |id_seq| seq | count |
    |---|------|-----|-------|
    | 0 |    0 | ATA |     2 |
    | 1 |    0 | AAA |     4 |
    | 2 |    0 | ATT |     3 |
    | 3 |    1 | ATT |     5 |
    | 4 |    1 | TTA |     1 |
    
    if we iterate from i=0..N and j=i..N (N = 5 in example), instead of
    i=0..N and j=0..N, we compute the upper triangle of the matrix but we
    need to be careful for diagonal elements K(i, i), because of the
    change in the second loop, for such coefficients, we need to multiply
    the number of matches by 2. This works well because the array
    is sorted by sequence id.

--- lib/mismatch.cpp
+++ lib/mismatch.cpp
@@ -59,15 +59,19 @@ void mismatch_compute_rec(sq_matrix<dtype> &K,
         // This is a leaf !
         // std::sort(tracks, tracks + len, compare<hash_t>);
 
+        // NOTE(RL) Normally, we would iterate for i=0..size and j=0..size
+        // but in fact we can iterator for j=i..size, and multiply
+        // by a coefficient (coeff) to compensate
         for (int i = 0; i < tracks.size(); i++) {
-            for (int j = 0; j < tracks.size(); j++) {
+            for (int j = i; j < tracks.size(); j++) {
 
                 int id1 = tracks[i].seq_id;
                 int id2 = tracks[j].seq_id;
 
                 int matches = tracks[i].count * tracks[j].count;
 
-                K(id1, id2) += matches;
+                int coeff = (i != j && id1 == id2) ? 2 : 1;
+                K(id1, id2) += coeff * matches;
             }
         }
 
