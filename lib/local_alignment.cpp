27f2fa03b4d32365c3b25d300276ce8d4778ecd3
Rémi Lespinet <remi@lespi.net>
2018-03-24 03:29:09 +0100

    Add local alignment : Smith waterman and Needleman Wunsch

--- lib/local_alignment.cpp
+++ lib/local_alignment.cpp
@@ -1,84 +1,151 @@
 #include <cstring>
 #include <algorithm>
+#include <limits>
 
 #include "local_alignment.hpp"
 
-struct dynamic_cell {
-    float M;
-    float X;
-    float Y;
-    // float X2;
-    // float Y2;
-};
 
 template<typename T>
-const T& max4(const T&a, const T&b, const T&c, const T&d) {
+const T& max(const T&a, const T&b, const T&c, const T&d) {
     using std::max;
     return max(max(a, b), max(c, d));
 }
 
 template<typename T>
-const T& max3(const T&a, const T&b, const T&c) {
+const T& max(const T&a, const T&b, const T&c) {
     using std::max;
     return max(a, max(b, c));
 }
 
 template<typename T>
-const T& max2(const T&a, const T&b) {
+const T& max(const T&a, const T&b) {
     using std::max;
     return max(a, b);
 }
 
 template<typename dtype>
-inline dtype local_alignment_seq(const vector1D<ltype> &sequence1, const vector1D<ltype> &sequence2,
-                                 int alphabet_size, dtype substitution, dtype gap_open, dtype gap_extension) {
+struct dynamic_cell {
+    dtype M;
+    dtype X;
+    dtype Y;
+};
 
-    matrix<dynamic_cell> table(sequence1.size() + 1, sequence2.size() + 1);
-    // dynamic_cell *table = new dynamic_cell[(sequences_len + 1) * (sequences_len + 1)];
+template<typename dtype>
+inline dtype needleman_wunsch_affine_gap(const vector1D<ltype> &sequence1,
+                                         const vector1D<ltype> &sequence2,
+                                         int alphabet_size, dtype substitution,
+                                         dtype gap_open, dtype gap_extension) {
 
-    for (int j = 0; j < sequence2.size() + 1; j++) {
-        table(0, i) = {0.0f, 0.0f, 0.0f};
+    using cell_type = dynamic_cell<float>;
+
+    // TODO(RL) Do something !
+    // if (sequence1.size() > sequence2.size()) {
+    //     std::swap(sequence1, sequence2);
+    // }
+
+    vector1D<cell_type> table1(sequence2.size() + 1, sequence2.size() + 1);
+    vector1D<cell_type> table2(sequence2.size() + 1, sequence2.size() + 1);
+
+    const dtype zero = dtype();
+    const dtype infm = std::numeric_limits<dtype>::min();
+    const dtype infp = std::numeric_limits<dtype>::max();
+
+    table1[0] = {zero, zero, zero};
+    for (int j = 0; j < sequence2.size(); j++) {
+        table1[j + 1] = {infm, - (j + 1) * gap_extension, infm};
     }
 
-    for (int i = 0; i < sequence1.size() + 1; i++) {
-        table(i, 0) = {0.0f, 0.0f, 0.0f};
+    dtype best_score = std::numeric_limits<dtype>::min();
+    for (int i = 0; i < sequence1.size(); i++) {
+
+        table2[0] = {infm, infm, - (i + 1) * gap_extension};
+
+        for (int j = 0; j < sequence2.size(); j++) {
+
+            cell_type &cell = table2[j + 1];
+
+            dtype cost = (sequence1[i] == sequence2[j]) ? substitution : -4;
+
+            // TODO(RL) This formula I've derived myself (verify)
+            cell.M = max(table1[j].M + cost,
+                         table1[j].X + cost,
+                         table1[j].Y + cost);
+
+            cell.X = max(table1[j+1].X - gap_extension,
+                         table1[j+1].M - gap_open);
+
+            cell.Y = max(table2[j].Y - gap_extension,
+                         table2[j].M - gap_open);
+
+
+        }
+
+        std::swap(table1, table2);
+
     }
+
+    cell_type &result = table1[sequence2.size()];
+
+    return max(result.M, result.X, result.Y);
+
+}
+
+template<typename dtype>
+inline dtype smith_waterman_affine_gap(const vector1D<ltype> &sequence1,
+                                       const vector1D<ltype> &sequence2,
+                                       int alphabet_size, dtype substitution,
+                                       dtype gap_open, dtype gap_extension) {
+
+    using cell_type = dynamic_cell<float>;
+
+    // TODO(RL) Do something !
+    // if (sequence1.size() > sequence2.size()) {
+    //     std::swap(sequence1, sequence2);
+    // }
+
+    vector1D<cell_type> table1(sequence2.size() + 1, sequence2.size() + 1);
+    vector1D<cell_type> table2(sequence2.size() + 1, sequence2.size() + 1);
 
     const dtype zero = dtype();
 
+    for (int j = 0; j < sequence2.size() + 1; j++) {
+        table1[j] = {zero, zero, zero};
+    }
+
+    table2[0] = {zero, zero, zero};
+
+    dtype best_score = zero;
     for (int i = 0; i < sequence1.size(); i++) {
 
         for (int j = 0; j < sequence2.size(); j++) {
 
+            cell_type &cell = table2[j + 1];
 
-            dynamic_cell &cell = table(i + 1, j + 1);
+            dtype cost = (sequence1[i] == sequence2[j]) ? substitution : -4;
 
-            dtype cost = (sequence1[i] == sequence2[j]) ? substitution : -substitution;
+            // TODO(RL) This formula I've derived myself (verify)
+            cell.M = max(table1[j].M + cost,
+                         table1[j].X + cost,
+                         table1[j].Y + cost,
+                         zero);
 
-            cell.M = max4(table(i, j).M + cost,
-                          table(i, j).X + cost,
-                          table(i, j).Y + cost,
-                          zero);
+            cell.X = max(table1[j+1].X - gap_extension,
+                         table1[j+1].M - gap_open);
 
-            cell.X = max3(table(i, j + 1).X - gap_extension,
-                          table(i, j + 1).M - gap_open,
-                          zero);
+            cell.Y = max(table2[j].Y - gap_extension,
+                         table2[j].M - gap_open);
 
-            cell.Y = max3(table(i + 1, j).Y - gap_extension,
-                          table(i + 1, j).M - gap_open,
-                          zero);
+            best_score = max(best_score, cell.M, cell.X, cell.Y);
 
         }
 
+        std::swap(table1, table2);
+
     }
 
-    const dynamic_cell &result = table(sequence1.size(), sequence2.size());
-
-    return max3(result.M, result.X, result.Y);
+    return best_score;
 
 }
-
-
 
 template<typename dtype>
 sq_matrix<dtype> local_alignment(const vector2D<ltype> &sequences,
@@ -92,9 +159,8 @@ sq_matrix<dtype> local_alignment(const vector2D<ltype> &sequences,
 
         for (int j = i; j < sequences.size(); j++) {
 
-            K(i, j) = local_alignment_seq(sequences[i], sequences[j], alphabet_size,
-                                          substitution, gap_open, gap_extension);
-
+            K(i, j) = needleman_wunsch_affine_gap(sequences[i], sequences[j], alphabet_size,
+                                                  substitution, gap_open, gap_extension);
         }
     }
 
@@ -104,5 +170,5 @@ sq_matrix<dtype> local_alignment(const vector2D<ltype> &sequences,
 }
 
 
-template sq_matrix<double> local_alignment(const vector2D<ltype> &, int, int, double, double, double);
+// template sq_matrix<double> local_alignment(const vector2D<ltype> &, int, int, double, double, double);
 template sq_matrix<float> local_alignment(const vector2D<ltype> &, int, int, float, float, float);
